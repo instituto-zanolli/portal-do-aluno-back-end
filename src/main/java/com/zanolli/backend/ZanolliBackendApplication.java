b6e17e62ec8edf512dc67e535d16a8ea26067fdd
raphaelkauan <raphaelkauanoficial@gmail.com>
2025-04-06 00:28:14 -0400

    first commit

---
+++ src/main/java/com/zanolli/backend/ZanolliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zanolli.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanolliBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZanolliBackendApplication.class, args);
+	}
+
+}
